ef5ce3098134e5ece94d6484e7e317e4af7040a9
hiralsutariya97 <hiralsutariya97@gmail.com>
2023-10-09 22:17:52 +0100

    homeworkweek5

---
+++ src/week5/BioData.java
@@ -0,0 +1,20 @@
+package week5;
+
+public class BioData {
+    public static void main(String[] args) {
+        System.out.println("Name: Pahal");
+        System.out.println("Surname: Patel");
+        System.out.println("BOD: 9 July 2000");
+        System.out.println("Hobby: Drawing,Travelling");
+        System.out.println("Address: Harrow");
+        System.out.println("Postcode: HA1 4HN");
+        System.out.println("Education: Fashion Designer");
+        System.out.println("Height: 5.6");
+        System.out.println("Nationality: Indian");
+        System.out.println("Language: English,Hindi,Gujarati");
+
+
+    }
+
+
+}
